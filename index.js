339cf5a319809cad5fb5eb88f00c410431fdbb5c
github username <github email address>
2021-03-30 14:42:18 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    const cards = [];
+    for (let i = 0; i < names.length; i++){
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return cards;
+}
+function countDown(num) {
+    while(num>=0){
+        console.log(num);
+        num--;
+    }
+}
